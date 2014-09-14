83845a5c3ae1c51a4d04e41a7e829d95959b7756
Wayne Krug <wmkrug@gmail.com>
2014-09-14 13:38:59 -0500

    Adding first source files

---
+++ src/fixed/fixed_linked_list.h
@@ -0,0 +1,22 @@
+#ifndef FIXED_LINKED_LIST_H
+#define FIXED_LINKED_LIST_H
+
+#include <inttypes.h>
+
+typedef struct FIXED_LL_NODE
+{
+    struct FIXED_LL_NODE *prev;
+    struct FIXED_LL_NODE *next;
+    void *payload;
+} FIXED_LL_NODE;
+
+typedef struct FIXED_LL
+{
+    FIXED_LL_NODE *memory_block;
+    FIXED_LL_NODE *head;
+    FIXED_LL_NODE *free;
+    uint32 main_list_length;
+    uint32 free_list_length;
+} FIXED_LL;
+
+#endif // FIXED_LINKED_LIST_H
