9aeb057b23df6f12ec371702457d353f37ab4e67
Wayne Krug <wmkrug@gmail.com>
2022-07-30 08:44:17 -0400

    Commiting a change I just found

--- src/fixed/fixed_linked_list.h
+++ src/fixed/fixed_linked_list.h
@@ -12,23 +12,15 @@ typedef struct FIXED_LL_NODE
 
 typedef struct FIXED_LL
 {
-<<<<<<< HEAD
     FIXED_LL_NODE *node_memory_block;
     char *data_memory_block;
-    FIXED_LL_NODE *head;
-    FIXED_LL_NODE *free;
-    uint32 main_list_length;
-    uint32 free_list_length;
-    size_t element_size;
-=======
-    FIXED_LL_NODE *memory_block;
     FIXED_LL_NODE *main_head;
-	FIXED_LL_NODE *main_tail;
+    FIXED_LL_NODE *main_tail;
     FIXED_LL_NODE *free;
-	uint32_t num_elements;
     uint32_t main_list_length;
     uint32_t free_list_length;
->>>>>>> 8e64a9d6938ff01169a7da25002d3dda70f9c59b
+    uint32_t num_elements;
+    size_t element_size;
 } FIXED_LL;
 
 #endif // FIXED_LINKED_LIST_H
